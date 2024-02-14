6a9c47ea837656ca848db690e68833f82f3b368d
baxtiyor <0898baxti@mail.ru>
2024-02-14 21:16:14 +0500

    user page add and nav-link component

---
+++ resources/js/Pages/User.vue
@@ -0,0 +1,27 @@
+<script setup>
+import Nav from "@/Shared/Nav";
+import {Link} from "@inertiajs/inertia-vue3";
+import Layout from "@/Shared/Layout.vue";
+defineProps({
+    time:String
+})
+</script>
+
+<template>
+   <Layout>
+       <h1>
+           User
+       </h1>
+
+       <div class="mg">
+           <p>The current time is {{time}}</p>
+           <Link :href="route('users')" text="Refresh" class="text-blue-500" preserve-scroll/>
+       </div>
+   </Layout>
+</template>
+
+<style scoped>
+  .mg {
+      margin-top: 500px;
+  }
+</style>
---
+++ resources/js/Shared/NavLink.vue
@@ -0,0 +1,15 @@
+<script setup>
+    import {Link} from "@inertiajs/inertia-vue3";
+    const props = defineProps({
+        active: Boolean
+    })
+</script>
+
+<template>
+    <Link
+        class="text-blue-500 hover:underline"
+        :class="{'font-bold underline': active}"
+    >
+        <slot/>
+    </Link>
+</template>
